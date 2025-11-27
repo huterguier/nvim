5369876e8514faddd865be93423f1e4e3652b66e
huterguier <huterguier@gmail.com>
2025-11-27 01:34:52 +0100

    .

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 5
 vim.opt.wrap = false
 vim.opt.tabstop = 4
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.winborder = 'rounded'
+vim.api.nvim_create_autocmd('TextYankPost', {
+	callback = function() vim.highlight.on_yank({ timeout = 200 }) end,
+})
 
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>so', ':update<CR> :source<CR>')
@@ -17,17 +21,17 @@ vim.keymap.set('n', '<leader>"', ':sp<CR>')
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('i', 'kj', '<Esc>')
 
-vim.pack.add({'https://github.com/christoomey/vim-tmux-navigator'})
+vim.pack.add({ 'https://github.com/christoomey/vim-tmux-navigator' })
 vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<CR>')
 vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<CR>')
 vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<CR>')
 vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<CR>')
 
-vim.pack.add({'https://github.com/stevearc/oil.nvim'})
+vim.pack.add({ 'https://github.com/stevearc/oil.nvim' })
 require 'oil'.setup()
 vim.keymap.set('n', '<leader>o', ':Oil<CR> --float')
 
-vim.pack.add({'https://github.com/github/copilot.vim',})
+vim.pack.add({ 'https://github.com/github/copilot.vim', })
 
 vim.pack.add {
 	{ src = 'https://github.com/neovim/nvim-lspconfig' },
@@ -46,9 +50,15 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 
+vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' })
+require('nvim-treesitter.configs').setup({
+	ensure_installed = { 'python', 'lua', 'typescript', 'javascript', 'html', 'css' },
+	highlight = { enable = true },
+})
+
 vim.pack.add({
-		{ src = 'https://github.com/nvim-telescope/telescope.nvim', tag = '0.1.9' },
-		'https://github.com/nvim-lua/plenary.nvim'
+	{ src = 'https://github.com/nvim-telescope/telescope.nvim', tag = '0.1.9' },
+	'https://github.com/nvim-lua/plenary.nvim'
 })
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
@@ -56,18 +66,28 @@ vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-vim.pack.add({'https://github.com/vague2k/vague.nvim'})
-vim.cmd('colorscheme vague')
+-- vim.pack.add({ 'https://github.com/vague2k/vague.nvim' })
+-- require('vague').setup({
+-- 	bold = false,
+-- 	italic = false,
+-- })
+-- vim.cmd('colorscheme vague')
 
-vim.pack.add({'https://github.com/stevearc/dressing.nvim'})
+vim.pack.add({ 'https://github.com/folke/tokyonight.nvim' })
+vim.cmd('colorscheme tokyonight-night')
+
+vim.pack.add({ 'https://github.com/stevearc/dressing.nvim' })
 require("dressing").setup({
-  input = { start_in_insert = false, insert_only = false, },
+	input = { start_in_insert = false, insert_only = false, },
 })
 
-vim.pack.add({'https://github.com/stevearc/conform.nvim'})
+vim.pack.add({ 'https://github.com/stevearc/conform.nvim' })
 require("conform").setup({
-  formatters_by_ft = {
-    python = { "isort", "black" },
-	typescript = { "prettierd" },
-  },
+	formatters_by_ft = {
+		python = { "isort", "black" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
